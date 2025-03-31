b46fde2d12a1d43bb1a592ab2549e85970ab6e9c
ayscd <ayeskacsta@gmail.com>
2025-03-30 22:50:58 -0300

    FINISH - end commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let button1 = document.querySelector("#challenge-1");
+let button2 = document.querySelector("#challenge-2");
+let button3 = document.querySelector("#challenge-3");
+
+button1.setAttribute("disabled", "disabled");
+button2.style.backgroundColor = "green";
+button3.classList = "danger";
